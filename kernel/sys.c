4466af440c91d953c4ecadafa0239a02fd5db879
Tim Zimmermann <tim@linux4.de>
2024-09-13 17:51:55 +0200

    kernel: bpf: Pretend to support ANY prog_type/attach_type
    * Also guard the entire workaround behind a config
    
    [huexxx@gmail.com: add \n to the end of all new pr_err()]
    Change-Id: Iae454ae98c54fb02f51f07a9683b9814a97b19a3

--- kernel/sys.c
+++ kernel/sys.c
@@ -1149,6 +1149,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
@@ -1156,6 +1157,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
