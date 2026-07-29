cc9fd86bbaf6f412ce0f32ae8d1793773fbb9df1
jzc233 <63482703+jzc233@users.noreply.github.com>
2026-07-29 13:26:16 +0800

    Change ctx type in BPF program definition

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -1614,7 +1614,7 @@ out:
 
 #define PROG_NAME(stack_size) __bpf_prog_run##stack_size
 #define DEFINE_BPF_PROG_RUN(stack_size) \
-static unsigned int PROG_NAME(stack_size)(const struct sk_buff *ctx, const struct bpf_insn *insn) \
+static unsigned int PROG_NAME(stack_size)(const void *ctx, const struct bpf_insn *insn) \
 { \
 	u64 stack[stack_size / sizeof(u64)]; \
 	u64 regs[MAX_BPF_EXT_REG]; \
